c60e4f474e1734795fba786210841a1e22ff0936
ferggz <ferggz.dev@gmail.com>
2026-04-22 12:51:00 +0200

    v3

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
-teams = [
-    {"id": 1, "name": "Arsenal", "logo": "arsenal.png", "pj": 8, "pg": 8, "pe": 0, "pp": 0, "gf": 23, "ga": 4, "points": 24},
-    {"id": 2, "name": "Bayern München", "logo": "bayern.png", "pj": 8, "pg": 7, "pe": 0, "pp": 1, "gf": 22, "ga": 8, "points": 21},
-    {"id": 3, "name": "Liverpool", "logo": "liverpool.png", "pj": 8, "pg": 6, "pe": 0, "pp": 2, "gf": 20, "ga": 8, "points": 18},
-    {"id": 4, "name": "Tottenham","logo": "tottenham.png", "pj": 8, "pg": 5, "pe": 2, "pp": 1, "gf": 17, "ga": 7, "points": 17},
-    {"id": 5, "name": "Barcelona", "logo": "barcelona.png", "pj": 8, "pg": 5, "pe": 1, "pp": 2, "gf": 22, "ga": 14, "points": 16},
-    {"id": 6, "name": "Chelsea", "logo": "chelsea.png", "pj": 8, "pg": 5, "pe": 1, "pp": 2, "gf": 17, "ga": 10, "points": 16},
-    {"id": 7, "name": "Sporting CP", "logo": "sporting.png", "pj": 8, "pg": 5, "pe": 1, "pp": 2, "gf": 17, "ga": 11, "points": 16},
-    {"id": 8, "name": "Manchester City", "logo": "city.png", "pj": 8, "pg": 5, "pe": 1, "pp": 2, "gf": 15, "ga": 9, "points": 16},
-    {"id": 9, "name": "Real Madrid", "logo": "realmadrid.png", "pj": 8, "pg": 5, "pe": 0, "pp": 3, "gf": 21, "ga": 12, "points": 15},
-    {"id": 10, "name": "Inter", "logo": "inter.png", "pj": 8, "pg": 5, "pe": 0, "pp": 3, "gf": 15, "ga": 7, "points": 15}
-]
+def load_teams():
+    with open("teams.json", encoding="utf-8") as f:
+        return json.load(f)
 
 @app.route("/")
 def home():
+    teams = load_teams()
+
+    query = request.args.get("q")
+
+    if query:
+        teams = [team for team in teams if query.lower() in team["name"].lower()]
+
     sorted_teams = sorted(
         teams, 
         key=lambda team: (
@@ -30,6 +29,8 @@ def home():
 
 @app.route("/team/<int:team_id>")
 def team(team_id):
+    teams = load_teams()
+
     for t in teams:
         if t["id"] == team_id:
             return render_template("team.html", team=t)
